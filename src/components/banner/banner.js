b2dff8c2ea797aa5e89d802f864c62c0c7b2762a
EthanHunt0001 <nijaskhan93@gmail.com>
2023-04-24 00:54:05 +0530

    banner play-button bug fixed

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './banner.css';
 import { API_KEY, IMG_URL } from '../../constants/constants';
 import axios from '../../tmdbAxios';
+import YouTube from 'react-youtube';
 
 function Banner() {
     const [movies, setMovie] = useState();
     const [selector, setSelector] = useState(0);
+    const [movieId, setMovieId] = useState();
 
     useEffect(() => {
       axios.get(`trending/all/day?api_key=${API_KEY}`)
@@ -15,18 +17,34 @@ function Banner() {
         setSelector(Math.floor(Math.random() * parseInt(MovieCount)) + 1);
       })
     }, [])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          autoplay: 1,
+        }
+    }
+    const handletrailer=(id)=>{
+        axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+        .then((response)=>{
+            setMovieId(response.data.results[0]);
+        })
+    }
     
     return (
-        <div className='banner' style={{backgroundImage: `url(${movies ? IMG_URL+movies[selector].backdrop_path: ""})`}}>
-            <div className='content'>
-                <h1 className='title'>{movies ? movies[selector].title : ""}</h1>
-                <div className='banner_buttons' style={{paddingBottom: '10px',paddingTop: '6px'}}>
-                    <button className='button' >Play</button>
-                    <button className='button' >My list</button>
+        <div>
+            <div className='banner' style={{backgroundImage: `url(${movies ? IMG_URL+movies[selector].backdrop_path: ""})`}}>
+                <div className='content'>
+                    <h1 className='title'>{movies ? movies[selector].title : ""}</h1>
+                    <div className='banner_buttons' style={{paddingBottom: '10px',paddingTop: '6px'}}>
+                        <button className='button' onClick={()=>handletrailer(movies[selector].id)}>Play</button>
+                        <button className='button'>My list</button>
+                    </div>
+                    <h1 className='description'>{movies ? movies[selector].overview : ""}</h1>
                 </div>
-                <h1 className='description'>{movies ? movies[selector].overview : ""}</h1>
+                <div className="fade_bottom"></div>
             </div>
-            <div className="fade_bottom"></div>
+            { movieId && <YouTube videoId={movieId.key} opts={opts} /> }
         </div>
     )
 }
